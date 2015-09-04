891fb3d1d07a6e869b319495736676e04d39a9a5
David Jackson <altmail@jackson-wallace.net>
2015-09-04 11:19:03 -0500

    Added whitelist and new tests.

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ var TELIZE_URL = 'http://www.telize.com/geoip';
 
 var isErrorStatus = function(statusCode) {
   return 400 <= statusCode && statusCode <= 500;
-}
+};
+
+var matchesWhitelist = function(path, regexes) {
+  return regexes.some((regex) => regex.test(path));
+};
 
 module.exports = function(options) {
   options = options || {};
 
   var serviceURL = options.serviceURL || TELIZE_URL;
+  var whitelist = options.pathRegexWhitelist;
 
   return function *(next) {
+    if (whitelist && !matchesWhitelist(this.path, whitelist)) {
+      //we have a whitelist, and this path doesn't match it
+      //skip the rest of the middleware
+      return yield next;
+    }
+
     var ip = this.request.ip || this.request.ips[0];
 
     if (ip) {
--- test/index.js
+++ test/index.js
@@ -10,24 +10,35 @@ var app;
 //place the HTTP fixtures inside the test directory
 Replay.fixtures = __dirname + '/fixtures';
 
+
+var serveGeoMiddleware = function *() {
+  this.response.type = 'application/json';
+
+  if (!this.state.geo) {
+    this.response.body = {
+      message: 'Geolocation info not available.'
+    };
+    return;
+  }
+
+  if (this.state.geo.error) {
+    this.response.body = this.state.geo.error;
+    return
+  }
+
+  this.response.body = this.state.geo;
+};
+
+
 describe('the geoip middleware', function() {
   beforeEach(function() {
     app = koa();
     app.proxy = true;
     app.use(geoip());
-    app.use(function *() {
-      this.response.type = 'application/json';
-
-      if (this.state.geo.error) {
-        this.response.body = this.state.geo.error;
-        return
-      }
-
-      this.response.body = this.state.geo;
-    });
+    app.use(serveGeoMiddleware);
   });
 
-  it('populates the state.geo variable if lookup is successful', function () {
+  it('populates the state.geo variable if lookup is successful', function() {
     return request(app.listen())
     .get('/')
     .set('X-Forwarded-For', '216.58.216.196')
@@ -39,7 +50,7 @@ describe('the geoip middleware', function() {
     });
   });
 
-  it('stores an error if lookup is unsuccessful', function () {
+  it('stores an error if lookup is unsuccessful', function() {
     return request(app.listen())
     .get('/')
     .set('X-Forwarded-For', 'not-a-valid-ip')
@@ -51,5 +62,60 @@ describe('the geoip middleware', function() {
         'Input string is not a valid IP address');
     });
   });
+
+  it('filters paths by regex if a whitelist is passed', function() {
+    app = koa();
+    app.proxy = true;
+    app.use(geoip({
+      pathRegexWhitelist: [/foo/]
+    }));
+    app.use(serveGeoMiddleware);
+
+    var server = app.listen();
+
+    return request(server)
+    .get('/foo')
+    .set('X-Forwarded-For', '216.58.216.196')
+    .expect(200)
+    .then((res) => {
+      assert.strictEqual(res.body.ip, '216.58.216.196');
+
+      return request(server)
+      .get('/bar')
+      .set('X-Forwarded-For', '216.58.216.196')
+      .expect(200);
+    })
+
+    .then((res) => {
+      assert.strictEqual(
+        res.body.message,
+        'Geolocation info not available.');
+    });
+  });
+
+  it('uses an alternate Telize API if one is passed', function() {
+    app = koa();
+    app.proxy = true;
+    app.use(geoip({
+      serviceURL: 'http://telize.com/foobar'
+    }));
+    app.use(serveGeoMiddleware);
+
+    var server = app.listen();
+
+    return request(server)
+    .get('/')
+    .set('X-Forwarded-For', '216.58.216.196')
+    .expect(200)
+    .then((res) => {
+      //the Telize path we specified is invalid, so we expect to get a 404 and
+      //no server-specified error message
+      assert.strictEqual(res.body.statusCode, 404);
+      assert.strictEqual(res.body.message, null);
+    });
+
+    
+  });
+
 
 });
